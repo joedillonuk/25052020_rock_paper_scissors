967de5d71c6741270b41ebd072245f43abfc8cbf
joedillonuk <joe.dillon.uk@gmail.com>
2020-05-25 14:59:09 +0100

    initial commit. Just a copy of today's Calculator work.

---
+++ rock_paper_scissors/controller.rb
@@ -0,0 +1,35 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry-byebug')
+
+require_relative('./models/calculator')
+also_reload('./models/*')
+
+get '/' do
+  erb(:home)
+end
+
+get '/about_us' do
+  erb(:about_us)
+end
+
+
+get '/add/:num1/:num2' do
+  @calculation = Calculator.add(params[:num1].to_i,params[:num2].to_i)
+erb(:result)
+end
+
+get '/subtract/:num1/:num2' do
+  @calculation = Calculator.subtract(params[:num1].to_i,params[:num2].to_i)
+erb(:result)
+end
+
+get '/multiply/:num1/:num2' do
+  @calculation = Calculator.multiply(params[:num1].to_i,params[:num2].to_i)
+erb(:result)
+end
+
+get '/divide/:num1/:num2' do
+  @calculation = Calculator.divide(params[:num1].to_i,params[:num2].to_i)
+erb(:result)
+end
